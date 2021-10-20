d99e2be3144f0dcc23ea921bdd9f29e6540150ed
eileen3110 <43286581+eileen3110@users.noreply.github.com>
2021-10-20 13:06:47 -0700

    Add files via upload

---
+++ 000.js
@@ -0,0 +1,7 @@
+// <!--  make "office" button clickable -->
+
+$(document).ready(function() { 
+	$('.header-nav-folder-title[href="/office-1"]').click(function() {
+		window.location = "/office";
+	});
+}); 
